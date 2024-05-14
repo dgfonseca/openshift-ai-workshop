934a38d955c608a4e8e5a9986cae696232f9dc63
David Fonseca <dafonsec@redhat.com>
2024-05-14 12:29:49 -0500

    [Fix] Added url

--- front/demo-retail/src/api/api.js
+++ front/demo-retail/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 async function executeModel(body){
-    return await axios.post('http://demo-api.demo-project.svc.cluster.local:8080/predict',body,{headers:{"Access-Control-Allow-Origin":"*"}});
+    return await axios.post('http://demo-api-demo-project.apps.cluster-k6txc.sandbox2369.opentlc.com/predict',body,{headers:{"Access-Control-Allow-Origin":"*"}});
 }
 
 export {executeModel}
